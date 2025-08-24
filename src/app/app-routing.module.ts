b7c505f3a3463bd2236aff2722332829efafa098
ranjan <ranjan.smth4@gmail.com>
2025-08-24 23:05:35 +0530

    Added component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+import { WelcomeComponent } from './welcome/welcome/welcome.component';
+import { AuthGuard } from './service/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent } // ✅ fixed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
