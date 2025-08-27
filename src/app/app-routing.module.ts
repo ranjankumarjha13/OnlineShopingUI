428b886eea1a42440db5a9bb8a6ecde4d472e863
ranjan <ranjan.smth4@gmail.com>
2025-08-27 22:06:09 +0530

    Added client module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { WelcomeComponent } from './welcome/welcome/welcome.component';
-import { AuthGuard } from './service/auth.guard';
+import { ShoppingcartComponent } from './shopping/shoppingcart/shoppingcart.component';
+import { AuthGuard } from './service/auth.guard'; // make sure AuthGuard exists
+import { CheckoutitemComponent } from './checkoutitem/checkoutitem.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'login', component: LoginComponent } // ✅ fixed
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'shopping',
+    component: ShoppingcartComponent
+  },
+  {
+    path: 'checkout',
+    component: CheckoutitemComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
 import { WelcomeComponent } from './welcome/welcome/welcome.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShoppingcartComponent } from './shopping/shoppingcart/shoppingcart.component';
+import { FormsModule } from '@angular/forms';
+import { CheckoutitemComponent } from './checkoutitem/checkoutitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ShoppingcartComponent,
+    CheckoutitemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule ,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
